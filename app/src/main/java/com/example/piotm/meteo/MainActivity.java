77c3554534d0dae1145904492a9c2efbbb168ffc
P. Casse-Noisette <piot.melanie@gmail.com>
2018-05-11 17:33:48 +0200

    + ActionBar

--- app/src/main/java/com/example/piotm/meteo/MainActivity.java
+++ app/src/main/java/com/example/piotm/meteo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.piotm.meteo;
 
+import android.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public onClick(View view){
+        //Boite de dialogue
     }
 }
